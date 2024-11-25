80d4684c301d4c2fbaa4d331e77201f4427486e6
MThiện CNTT <thanhsooon1111@gmail.com>
2024-11-25 10:33:35 +0700

    Update Bai18_tinhsothoctrenobanco.cpp
    update

--- Bai18_tinhsothoctrenobanco.cpp
+++ Bai18_tinhsothoctrenobanco.cpp
@@ -1,23 +1,26 @@
-// Bai18_tinhsothoctrenobanco.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// bai_18Tinhsothoctrenbanco.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
-int main()
-{
-    cout << "|Tinh so hat thoc tren ban co|\n";
-    double o, tongthoc=0,tong=0;
-    cout << "nhap o :";
-    cin >> o; 
-    tongthoc = 0;
-    for (int i=1;i <= o; i++)
-    {
-        tongthoc = pow(2, i-1);
-        tong += tongthoc;
-        cout << "o " << i << ": " << tongthoc << endl; 
-    }
-    cout << "Tong so thoc : " << tong << endl;
-    return 0;
+int main() {
+	int SoO, sothoc;
+	cout << "nhap so O : \n"; cin >> SoO;
+	cout.imbue(locale("en_US.UFT-8")); 
+	double tongsothoc = 0;
+	sothoc = 0;
+	if (SoO > 64) {
+		cout << "tren ban co chi co 64 O , vui long nhap so <=64\n";
+		system("pause");
+		return 0;
+	}
+	for (int i = 0; i < SoO; i++)
+	{
+		tongsothoc = tongsothoc + pow(2, i);
+		cout << "othu" << i << ":" << tongsothoc << endl;
+	}
+	cout << " tong so thoc tren ban co: " << tongsothoc << endl;
+	cout << "tong so thoc tren ban co:" << fixed << setprecision(0) << tongsothoc << endl;
+	return 0;
 }
-
